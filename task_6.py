41bf6bf2b8ac38d1f2c8ff82d0a86bf48bfcf62b
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2025-02-07 13:26:44 +0000

    edit table to save used pcoducts

--- task_6.py
+++ task_6.py
@@ -10,7 +10,7 @@ def modify_items(items: dict):
     for name, value in items.items():
         cost = value["cost"]
         calories = value["calories"]
-        products[name] = (Item(calories, cost))
+        products[name] = Item(calories, cost)
     return products
 
 
@@ -23,46 +23,50 @@ def greedy_algorithm(items: dict, money: int) -> dict:
 
     for name, value in sorted_items.items():
         if current_money >= value.cost:
-            # num = current_money // value.cost
-            current_money -=  value.cost
+            current_money -= value.cost
             total_calories += value.calories
             total_products[name] = total_products.get(name, 0) + 1
-        
+
         if current_money == 0:
             break
 
-    return  total_calories, total_products, money - current_money
+    return total_calories, total_products, money - current_money
 
 
 def dynamic_programming(items: dict, money: int):
     items_values = list(items.values())
     items_names = list(items.keys())
-    used_items = [0] * (money + 1)
-    
+
+    # Table to calculate max number of calories
     K = [[0 for x in range(money + 1)] for y in range(len(items) + 1)]
 
-    for i in range(len(items) + 1):
-        for x in range(money + 1):
-            if i == 0 or x == 0:
-                K[i][x] = 0
-            elif items_values[i - 1].cost <= x:
-                used_items[x] = items_names[i - 1]
-                K[i][x] = max(items_values[i - 1].calories + K[i - 1][x - items_values[i - 1].cost], K[i - 1][x])
+    # Table to save used products
+    keep = [[0 for x in range(money + 1)] for y in range(len(items) + 1)]
+
+    for i in range(1, len(items) + 1):
+        for x in range(1, money + 1):
+            if items_values[i - 1].cost <= x:
+                # If product is included
+                if items_values[i - 1].calories + K[i - 1][x - items_values[i - 1].cost] > K[i - 1][x]:
+                    K[i][x] = items_values[i - 1].calories + K[i - 1][x - items_values[i - 1].cost]
+                    keep[i][x] = i - 1
+                else:
+                    K[i][x] = K[i - 1][x]
             else:
                 K[i][x] = K[i - 1][x]
 
     total_items = {}
     current_money = money
+    i = len(items)
 
-    print(used_items)
+    while i >= 0 and current_money > 0:
+        if keep[i][current_money] != keep[i - 1][current_money]:
+            product = items_names[keep[i][current_money]]
+            total_items[product] = total_items.get(product, 0) + 1
+            current_money -= items[product].cost
+        i -= 1
 
-    while current_money > 0:
-        product = used_items[current_money]
-        total_items[product] = total_items.get(product, 0) + 1
-        current_money -= items[product].cost
-
-
-    return K[len(items)][money], money - current_money, total_items
+    return K[len(items)][money], total_items, money - current_money
 
 
 items = {
